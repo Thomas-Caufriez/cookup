cd94eb8a19037a1f3944175ae347d7371c158c6d
Thomas-Caufriez <thcaufriez@gmail.com>
2024-12-08 12:05:16 +0100

    page connexion responsive
    ajout d'attributs responsive sur la page connexion

--- page-login.php
+++ page-login.php
@@ -20,59 +20,61 @@ get_header('bis');
     action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>"
     method="post"
   >
-    <div class="mb-3">
-      <label 
-        for="log" 
-        class="form-label"
-      >
-        Email ou nom d'utilisateur
-      </label>
-      <input 
-        type="text" 
-        name="log"
-        class="form-control" 
-        id="log" 
-        placeholder="Ex : John@doe.com"
-        required
-        value="<?php echo esc_attr( $user_login ); ?>"
-      >
+    <div class="row row-cols-1 row-cols-md-2">
+      <div class="mb-3 col">
+        <label 
+          for="log" 
+          class="form-label"
+        >
+          Email ou nom d'utilisateur
+        </label>
+        <input 
+          type="text" 
+          name="log"
+          class="form-control" 
+          id="log" 
+          placeholder="Ex : John@doe.com"
+          required
+          value="<?php echo esc_attr( $user_login ); ?>"
+        >
+      </div>
+      <div class="mb-3 col">
+        <label 
+          for="pwd" 
+          class="form-label"
+        >
+          Mot de passe
+        </label>
+        <input 
+          type="password" 
+          name="pwd"
+          class="form-control" 
+          id="pwd"
+          placeholder="Ex : lesCr3at3ursDeC0okupSontL3s+Beaux"
+          required
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+        >
+          <div>
+            <a class="small" href="<?php echo esc_url( home_url('/connexion/mot-de-passe-oublie') ); ?>">Mot de passe oublié ?</a>
+          </div>
+      </div>
     </div>
-    <div class="mb-3">
-      <label 
-        for="pwd" 
-        class="form-label"
-      >
-        Mot de passe
-      </label>
+    <div class="text-center">
       <input 
-        type="password" 
-        name="pwd"
-        class="form-control" 
-        id="pwd"
-        placeholder="Ex : lesCr3at3ursDeC0okupSontL3s+Beaux"
-        required
-        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+        type="submit" 
+        name="submit" 
+        value="Connexion"
+        class="btn"
       >
-      </div>
-      <div>
-        <a class="small" href="<?php echo esc_url( home_url('/connexion/mot-de-passe-oublie') ); ?>">Mot de passe oublié ?</a>
-      </div>
-      <div class="text-center">
-        <input 
-          type="submit" 
-          name="submit" 
-          value="Connexion"
-          class="btn"
-        >
-        <img 
-          src="<?php echo get_template_directory_uri(); ?>/assets/img/" 
-          alt="login"
-        >
-        <input 
-          type="hidden" 
-          name="redirect_to" 
-          value="<?php echo esc_url( home_url('/profil') ); ?>"
-        >
+      <img 
+        src="<?php echo get_template_directory_uri(); ?>/assets/img/" 
+        alt="login"
+      >
+      <input 
+        type="hidden" 
+        name="redirect_to" 
+        value="<?php echo esc_url( home_url('/profil') ); ?>"
+      >
     </div>
   </form>
 </div>
@@ -85,58 +87,62 @@ get_header('bis');
 
 <div class="container">
   <form method="post">
-    <div class="mb-3">
-      <label 
-        for="name" 
-        class="form-label"
-      >
-        Nom d'utilisateur
-      </label>
-      <input 
-        type="text" 
-        name="uname"
-        class="form-control" 
-        id="name" 
-        placeholder="Ex : John"
-        required
-      >
+    <div class="row row-cols-1 row-cols-md-2">
+      <div class="mb-3 col">
+        <label 
+          for="name" 
+          class="form-label"
+        >
+          Nom d'utilisateur
+        </label>
+        <input 
+          type="text" 
+          name="uname"
+          class="form-control" 
+          id="name" 
+          placeholder="Ex : John"
+          required
+        >
+      </div>
+      <div class="mb-3 col">
+        <label 
+          for="email" 
+          class="form-label"
+        >
+          Email
+        </label>
+        <input 
+          type="text" 
+          name="uemail"
+          class="form-control" 
+          id="email" 
+          placeholder="Ex : John@doe.com"
+          required
+          pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+        >
+      </div>
     </div>
-    <div class="mb-3">
-      <label 
-        for="email" 
-        class="form-label"
-      >
-        Email
-      </label>
-      <input 
-        type="text" 
-        name="uemail"
-        class="form-control" 
-        id="email" 
-        placeholder="Ex : John@doe.com"
-        required
-        pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
-      >
-    </div>
-    <div class="mb-3">
-      <label 
-        for="pwd" 
-        class="form-label"
-      >
-        Mot de passe
-      </label>
-      <input 
-        type="password" 
-        name="upass"
-        class="form-control" 
-        id="pwd"
-        placeholder="Ex : lesCr3at3ursDeC0okupSontL3s+Beaux"
-        required
-        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
-      >
-    </div>
-    <div>
-      <p class="small">Le mot de passe doit contenir au minimum 6 caractères dont au moins une majuscule, une minuscule et un chiffre</p>
+    <div class="row">
+      <div class="mb-3 col">
+        <label 
+          for="pwd" 
+          class="form-label"
+        >
+          Mot de passe
+        </label>
+        <input 
+          type="password" 
+          name="upass"
+          class="form-control" 
+          id="pwd"
+          placeholder="Ex : lesCr3at3ursDeC0okupSontL3s+Beaux"
+          required
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+        >
+        <div>
+          <p class="small">Le mot de passe doit contenir au minimum 6 caractères dont au moins une majuscule, une minuscule et un chiffre</p>
+        </div>
+      </div>
     </div>
     <div class="text-center">
       <input 
