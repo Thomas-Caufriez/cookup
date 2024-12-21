35996c800ebc17c1affeb3292a1f265b6117755b
Jniouen-Wassim <jniouenwassim@gmail.com>
2024-12-21 20:12:50 +0100

    add fonction

--- functions.php
+++ functions.php
@@ -6,38 +6,33 @@ add_theme_support('menus');
 register_nav_menu('footer', 'footer');
 
 // load bootstrap, la(les) feuille(s) css, la(les) feuille(s) js
-  function styles_scripts()
-    {
-      wp_enqueue_style(
-        'bootstrap',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
-      );
-      wp_enqueue_style(
-        'style',
-        get_template_directory_uri() . '/assets/css/app.css'
-      );
+function styles_scripts()
+{
+  wp_enqueue_style(
+    'bootstrap',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+  );
+  wp_enqueue_style(
+    'style',
+    get_template_directory_uri() . '/assets/css/app.css'
+  );
 
-      wp_enqueue_script(
-        'jquery'
-      );
-
-      wp_enqueue_script(
-        'bootstrap-bundle',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
-        false,
-        1,
-        true
-      );
-
-      wp_enqueue_script(
-        'app-js',
-        get_template_directory_uri() . '/assets/js/app.js',
-        array('jquery', 'bootstrap'),
-        null,
-        true
-      );
-    }
-  add_action('wp_enqueue_scripts', 'styles_scripts');
+  wp_enqueue_script(
+    'bootstrap-bundle',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+    false,
+    1,
+    true
+  );
+  wp_enqueue_script(
+    'app-js',
+    get_template_directory_uri() . '/assets/js/app.js',
+    ['bootstrap-bundle'],
+    1,
+    true
+  );
+}
+add_action('wp_enqueue_scripts', 'styles_scripts');
 
 // ajoute la classe nav-item aux li placés par la fonction wp_nav_menu()
   function footer_menu_class($classes) {
