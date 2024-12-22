63114ae752b2a65641fed47f7e849eb28cf72330
Jniouen-Wassim <jniouenwassim@gmail.com>
2024-12-22 23:16:41 +0100

    page profil

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer class="mt-auto mx-0 container-fluid text-center bg-primary">
         <nav class="navbar navbar-expand">
             <div class="container-fluid">
-                <ul class="navbar-nav m-0 w-100 footer-textN1">
+                <ul class="navbar-nav mx-0 w-100  footer-textN1">
                     <?php  wp_nav_menu_no_ul(); ?>
                     <li class="nav-item col">
                         <a 
@@ -15,7 +15,7 @@
             </div>
         </nav>
         <div class="row footer-textN2">
-            <p class="m-0">Tous droits réservés à Cookup.be  ©2024</p>
+            <p class="mb-1">Tous droits réservés à Cookup.be  ©2024</p>
         </div>
     </footer>
     
--- page-profile.php
+++ page-profile.php
@@ -13,84 +13,87 @@ get_header('bis');
 $curent_user = wp_get_current_user();
 ?>
 
-<div class="container-fluid text-center">
+<div class="container-fluid text-center mt-4">
     <div class="row row-cols-1 row-cols-md-2">
-        <div class="col">
+        <div class="col ">
             <div class="row">
                 <div class="col-6">
                     <?php echo get_avatar($curent_user->ID, 96, 'identicon', 'photo de profil', array('class' => 'rounded-circle'))?>
                 </div>
                 <div class="col-6 text-start">
-                    <div>
+                    <div class="mt-2 h2Salsa">
                         <?php //au début j'ai voulu utiliser un foreach dans ma chaine et stocker le tout dans une variable pour la print mais j'ai trouvé implode
-                            echo $current_user->user_login . ' - ' . implode(' ', $current_user->roles); 
+                            echo $current_user->user_login . ' - <span style="background: #FF62C0;  -webkit-background-clip: text; -webkit-text-fill-color: transparent; font-weight: bold;">' . implode(' ', $current_user->roles) . '</span>'; 
                         ?>
                     </div>
-                    <div>
+                    <div class="mt-2 pRobotoLogin">
                         <?php echo $current_user->user_email; ?>
                     </div>
-                    <div>
-                        <a href="<?php echo home_url('/profil/modify-profil'); ?>">Modifier le profil</a>
+                    <div class="mt-2">
+                        <a href="<?php echo home_url('/profil/modify-profil'); ?>" class="pRobotoLogin">Modifier le profil</a>
                     </div>
-                    <div>
-                        <a href="<?php echo wp_logout_url('/login'); ?>">Se déconnecter</a>
+                    <div class="mt-2">
+                        <a href="<?php echo wp_logout_url('/login'); ?>"class="pRobotoLogin">Se déconnecter</a>
                     </div>
                 </div>
                 <div class="col-6 text-center">
                     <a 
                         href="<?php echo home_url('/create-recipe'); ?>"
-                        class="btn btn-primary"
+                        class="btn btnRecetteProfil"
                     >
                         <img 
-                            src="<?php echo get_template_directory_uri(); ?>/assets/img/" 
+                            src="<?php echo get_template_directory_uri(); ?>/assets/img/iconRecette.svg" 
                             alt="icone créer"
                         >
-                        <span>Crée une recette</span>
+                        <span class="pRobotoLogin">Crée une recette</span>
                     </a>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <button class="container-fluid btn btn-primary m-2">
+        <div class="col mt-3">
+            <button class="container-fluid btn mt-2 catégorieProfil">
                 <img 
-                    src="<?php echo get_template_directory_uri(); ?>/assets/img/" 
+                    src="<?php echo get_template_directory_uri(); ?>/assets/img/imgPublic.jpg" 
                     alt="personne entrain de se filmer pendant qu'elle cuisine"
-                    class="img-fluid"
+                    class="img-fluid imgPublic"
                 >
                 <div>
                     <img 
-                        src="<?php echo get_template_directory_uri(); ?>/assets/img/" 
+                        src="<?php echo get_template_directory_uri(); ?>/assets/img/iconPublic.svg" 
                         alt="icone groupe de personnes"
+                        class="iconPublic"
                     >
-                    <span>Public</span>
+                    <span class="pRobotoLogin">Public</span>
                 </div>
             </button>
-            <button class="container-fluid btn btn-primary m-2">
+            <button class="container-fluid btn mt-2 catégorieProfil">
                 <img 
-                    src="<?php echo get_template_directory_uri(); ?>/assets/img/" 
+                    src="<?php echo get_template_directory_uri(); ?>/assets/img/imgFavoris.jpg" 
                     alt="table avec des dessert dessus"
-                    class="img-fluid"
+                    class="img-fluid imgFavoris"
                 >
                 <div>
                     <img 
-                        src="<?php echo get_template_directory_uri(); ?>/assets/img/" 
+                        src="<?php echo get_template_directory_uri(); ?>/assets/img/iconFavoris.svg" 
                         alt="icone coeur"
+                        class="iconFavoris"
                     >
-                    <span>Favoris</span>
+                    <span class="pRobotoLogin">Favoris</span>
                 </div>
             </button>
-            <button class="container-fluid btn btn-primary m-2">
+            <button class="container-fluid btn mt-2 catégorieProfil">
                 <img 
-                    src="<?php echo get_template_directory_uri(); ?>/assets/img/" 
+                    src="<?php echo get_template_directory_uri(); ?>/assets/img/imgPrivé.jpg" 
                     alt="tablette avec un recette de cuisine affichée"
-                    class="img-fluid"
+                    class="img-fluid imgPrivé"
                 >
                 <div>
                     <img 
-                        src="<?php echo get_template_directory_uri(); ?>/assets/img/" 
+                        src="<?php echo get_template_directory_uri(); ?>/assets/img/iconPrivé.svg" 
                         alt="icone personne seule"
+                        class="iconPrivé"
                     >
-                    <span>Privé</span>
+                    <span class="pRobotoLogin">Privé</span>
                 </div>
             </button>
         </div>
