ebe0eb54226a8a5200bc23c9ca39c2ac89da198f
Thomas-Caufriez <thcaufriez@gmail.com>
2024-12-26 15:01:26 +0100

    navbar v2
    -caractère responsive (se mets au dessus en version desktop)
    -boutons du menu fonctionnent et ouvrent des menus cachés avec des options de fitres

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-console.log('test')
+console.log('app.js chargé');
 
 function changepwd() {
   const hidden = document.getElementById("pwd");
@@ -24,6 +24,7 @@ function sendmail() {
 
 //  cette fonction était sensé venir dans la création de recette pour display une image quand elle est choisie mais cela ne fonctionne pas
 //  car il ne trouve pas la fonction => fichier app.js mal lié ? pourtant les deux premières fonctions du fichier fonctionnent
+//  Correction ==> il faut vider le cache a chaque fois pour le js mais compris trop tard dans le projet donc pas le temps d'implémenter cette fonction
 function previewImage(event) {
   const files = event.target.files;
   if(files.lenght > 0) {
